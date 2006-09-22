8e35d4b76a057d4f745654e711e021fe20c261c6
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2006-09-22 01:44:18 +0000

    Rename role

--- UFL-Curriculum/trunk/db/default_users.sql
+++ UFL-Curriculum/trunk/db/default_users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username) VALUES ('dwc');
 
-INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('administrator');
 
-INSERT INTO user_roles (user_id, role_id) VALUES ((SELECT id FROM users WHERE username = 'dwc'), (SELECT id FROM roles WHERE name = 'admin'));
+INSERT INTO user_roles (user_id, role_id) VALUES ((SELECT id FROM users WHERE username = 'dwc'), (SELECT id FROM roles WHERE name = 'administrator'));
