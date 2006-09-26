41e8408c9c87b9830598d1d669fd34a76b96d406
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2006-09-26 03:32:01 +0000

    Basic group view

---
+++ UFL-Curriculum/trunk/lib/UFL/Curriculum/Controller/Groups.pm
@@ -0,0 +1,76 @@
+package UFL::Curriculum::Controller::Groups;
+
+use strict;
+use warnings;
+use base qw/UFL::Curriculum::BaseController/;
+
+=head1 NAME
+
+UFL::Curriculum::Controller::Groups - Groups controller component
+
+=head1 SYNOPSIS
+
+See L<UFL::Curriculum>.
+
+=head1 DESCRIPTION
+
+L<Catalyst> controller component for managing groups.
+
+=head1 METHODS
+
+=head2 index 
+
+Display a list of current groups.
+
+=cut
+
+sub index : Path Args(0) {
+    my ($self, $c) = @_;
+
+    my $groups = $c->model('DBIC::Group')->search(undef, { order_by => 'name' });
+
+    $c->stash(
+        groups   => $groups,
+        template => 'groups/index.tt',
+    );
+}
+
+=head2 group
+
+Fetch the specified group.
+
+=cut
+
+sub group : PathPart('groups') Chained('/') CaptureArgs(1) {
+    my ($self, $c, $group_id) = @_;
+
+    my $group = $c->model('DBIC::Group')->find($group_id);
+    $c->detach('/default') unless $group;
+
+    $c->stash(group => $group);
+}
+
+=head2 view
+
+Display basic information on the stashed group.
+
+=cut
+
+sub view : PathPart('') Chained('group') Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash(template => 'groups/view.tt');
+}
+
+=head1 AUTHOR
+
+Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
