8307c6c0171f702944eb59ccc941e8e5a4b92308
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2006-10-27 14:52:53 +0000

    Seed administrators in IT

--- UFL-Workflow/trunk/db/default_users.sql
+++ UFL-Workflow/trunk/db/default_users.sql
@@ -1,3 +1,5 @@
+INSERT INTO users (username) VALUES ('cschoaf');
+INSERT INTO users (username) VALUES ('mhoit');
 INSERT INTO users (username) VALUES ('dwc');
 
 INSERT INTO groups (name) VALUES ('Information Technology');
@@ -9,7 +11,24 @@ INSERT INTO groups (parent_group_id, name) VALUES (
 INSERT INTO roles (name) VALUES ('Administrator');
 
 INSERT INTO group_roles (group_id, role_id) VALUES (
+  (SELECT id FROM groups WHERE name = 'Information Technology'),
+  (SELECT id FROM roles WHERE name = 'Administrator')
+);
+
+INSERT INTO group_roles (group_id, role_id) VALUES (
   (SELECT id FROM groups WHERE name = 'Web Administration'),
+  (SELECT id FROM roles WHERE name = 'Administrator')
+);
+
+INSERT INTO user_group_roles (user_id, group_id, role_id) VALUES (
+  (SELECT id FROM users WHERE username = 'cschoaf'),
+  (SELECT id FROM groups WHERE name = 'Information Technology'),
+  (SELECT id FROM roles WHERE name = 'Administrator')
+);
+
+INSERT INTO user_group_roles (user_id, group_id, role_id) VALUES (
+  (SELECT id FROM users WHERE username = 'mhoit'),
+  (SELECT id FROM groups WHERE name = 'Information Technology'),
   (SELECT id FROM roles WHERE name = 'Administrator')
 );
 
