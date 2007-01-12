28c65fd3859df4c38f1657aa3dd0a32b10f2c8d9
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-01-12 03:53:52 +0000

    Add an initial process

--- UFL-Workflow/trunk/db/default_data.sql
+++ UFL-Workflow/trunk/db/default_data.sql
@@ -37,3 +37,27 @@ INSERT INTO user_group_roles (user_id, group_id, role_id) VALUES (
   (SELECT id FROM groups WHERE name = 'Web Administration'),
   (SELECT id FROM roles WHERE name = 'Administrator')
 );
+
+INSERT INTO statuses (name, is_initial) VALUES ('Pending', 1);
+INSERT INTO statuses (name, continues_request) VALUES ('Approved', 1);
+INSERT INTO statuses (name, finishes_request) VALUES ('Denied', 1);
+INSERT INTO statuses (name) VALUES ('Tabled');
+
+INSERT INTO processes (user_id, name) VALUES (
+  (SELECT id FROM users WHERE username = 'dwc'),
+  'Test'
+);
+
+INSERT INTO steps (process_id, role_id, name) VALUES (
+  (SELECT id FROM processes WHERE name = 'Test'),
+  (SELECT id FROM roles WHERE name = 'Administrator'),
+  'First'
+);
+INSERT INTO steps (process_id, role_id, name) VALUES (
+  (SELECT id FROM processes WHERE name = 'Test'),
+  (SELECT id FROM roles WHERE name = 'Administrator'),
+  'Second'
+);
+
+UPDATE steps SET next_step_id = (SELECT id FROM steps WHERE name = 'Second') WHERE id = (SELECT id FROM steps WHERE name = 'First');
+UPDATE steps SET prev_step_id = (SELECT id FROM steps WHERE name = 'First') WHERE id = (SELECT id FROM steps WHERE name = 'Second');
