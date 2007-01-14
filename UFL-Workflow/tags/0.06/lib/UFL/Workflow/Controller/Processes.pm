35ae263c5300aa721567e9266207915ead89b654
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-01-14 17:33:32 +0000

    Tagging 0.06

---
+++ UFL-Workflow/tags/0.06/lib/UFL/Workflow/Controller/Processes.pm
@@ -0,0 +1,270 @@
+package UFL::Workflow::Controller::Processes;
+
+use strict;
+use warnings;
+use base qw/UFL::Workflow::BaseController/;
+
+=head1 NAME
+
+UFL::Workflow::Controller::Processes - Processes controller component
+
+=head1 SYNOPSIS
+
+See L<UFL::Workflow>.
+
+=head1 DESCRIPTION
+
+L<Catalyst> controller component for managing processes.
+
+=head1 METHODS
+
+=head2 index 
+
+Display a list of current processes.
+
+=cut
+
+sub index : Path('') Args(0) {
+    my ($self, $c) = @_;
+
+    my $processes = $c->model('DBIC::Process')->search(undef, { order_by => 'name' });
+
+    $c->stash(
+        processes => $processes,
+        template  => 'processes/index.tt',
+    );
+}
+
+=head2 add
+
+Add a new process.
+
+=cut
+
+sub add : Local {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my $process = $c->user->processes->create({
+                name        => $result->valid('name'),
+                description => $result->valid('description'),
+            });
+
+            return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+        }
+    }
+
+    $c->stash(template => 'processes/add.tt');
+}
+
+=head2 process
+
+Fetch the specified process.
+
+=cut
+
+sub process : PathPart('processes') Chained('/') CaptureArgs(1) {
+    my ($self, $c, $process_id) = @_;
+
+    my $process = $c->model('DBIC::Process')->find($process_id);
+    $c->detach('/default') unless $process;
+
+    $c->stash(process => $process);
+}
+
+=head2 view
+
+Display basic information on the stashed process.
+
+=cut
+
+sub view : PathPart('') Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash(template => 'processes/view.tt');
+}
+
+=head2 edit
+
+Edit the stashed process.
+
+=cut
+
+sub edit : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my $process = $c->stash->{process};
+            $process->update({
+                name        => $result->valid('name'),
+                description => $result->valid('description'),
+            });
+
+            return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+        }
+    }
+
+    $c->stash(template => 'processes/edit.tt');
+}
+
+=head2 add_step
+
+Add a step to the stashed process.
+
+=cut
+
+sub add_step : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my $process = $c->stash->{process};
+
+            my $role = $c->model('DBIC::Role')->find($result->valid('role_id'));
+            $c->detach('/default') unless $role;
+
+            my $step = $process->add_step($result->valid('name'), $role);
+
+            return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+        }
+    }
+
+    my $roles = $c->model('DBIC::Role')->search(undef, {
+        order_by => 'name'
+    });
+
+    $c->stash(
+        roles    => $roles,
+        template => 'processes/add_step.tt',
+    );
+}
+
+=head2 delete_step
+
+Remove the specified step from the stashed process.
+
+=cut
+
+sub delete_step : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    die 'Method must be POST' unless $c->req->method eq 'POST';
+
+    my $process = $c->stash->{process};
+
+    my $result = $self->validate_form($c);
+    if ($result->success) {
+        my $step = $process->steps->find($result->valid('step_id'));
+        $c->detach('/default') unless $step;
+
+        $step->delete;
+    }
+
+    return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+}
+
+=head2 move_step_up
+
+Move the specified step up one position in the stashed process.
+
+=cut
+
+sub move_step_up : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    die 'Method must be POST' unless $c->req->method eq 'POST';
+
+    my $process = $c->stash->{process};
+
+    my $result = $self->validate_form($c);
+    if ($result->success) {
+        my $step = $process->steps->find($result->valid('step_id'));
+        $c->detach('/default') unless $step;
+
+        $step->move_up;
+    }
+
+    return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+}
+
+=head2 move_step_down
+
+Move the specified step down one position in the stashed process.
+
+=cut
+
+sub move_step_down : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    die 'Method must be POST' unless $c->req->method eq 'POST';
+
+    my $process = $c->stash->{process};
+
+    my $result = $self->validate_form($c);
+    if ($result->success) {
+        my $step = $process->steps->find($result->valid('step_id'));
+        $c->detach('/default') unless $step;
+
+        $step->move_down;
+    }
+
+    return $c->res->redirect($c->uri_for($self->action_for('view'), $process->uri_args));
+}
+
+=head2 add_request
+
+Add a request that follows the specified process.
+
+=cut
+
+sub add_request : PathPart Chained('process') Args(0) {
+    my ($self, $c) = @_;
+
+    my $process = $c->stash->{process};
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my $group = $c->model('DBIC::Group')->find($result->valid('group_id'));
+            $c->detach('/default') unless $group;
+
+            my $request = $process->add_request(
+                $result->valid('title'),
+                $result->valid('description'),
+                $c->user->obj,
+                $group,
+            );
+
+            return $c->res->redirect($c->uri_for($c->controller('Requests')->action_for('view'), $request->uri_args));
+        }
+    }
+
+    my @groups;
+    if (my $first_step = $process->first_step) {
+        @groups = $first_step->role->groups;
+    }
+
+    $c->stash(
+        process  => $process,
+        groups   => \@groups,
+        template => 'processes/add_request.tt',
+    );
+}
+
+=head1 AUTHOR
+
+Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
