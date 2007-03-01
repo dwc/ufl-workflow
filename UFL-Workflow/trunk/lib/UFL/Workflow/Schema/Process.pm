fc9791a3b35a4e6615bb4a8f89640b53bfec3af8
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-03-01 23:12:45 +0000

    Extend name columns to 64 characters

--- UFL-Workflow/trunk/lib/UFL/Workflow/Schema/Process.pm
+++ UFL-Workflow/trunk/lib/UFL/Workflow/Schema/Process.pm
@@ -15,7 +15,7 @@ __PACKAGE__->add_columns(
     },
     name => {
         data_type => 'varchar',
-        size      => 32,
+        size      => 64,
     },
     description => {
         data_type   => 'varchar',
