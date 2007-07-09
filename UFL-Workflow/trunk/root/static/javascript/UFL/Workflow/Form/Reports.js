47725e0df424e8e9fce7aab07f23c38ce111dec4
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-07-09 22:51:05 +0000

    Add basic type safety to the JavaScript

--- UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
+++ UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
@@ -1,12 +1,12 @@
 function setDate(dateRange) {
-    if(dateRange) {
+    if (dateRange) {
         var startDates = dateRange.split('-', 3);
-        document.forms[0].start_date_year.value  = startDates[0];
-        document.forms[0].start_date_month.value = startDates[1];
-        document.forms[0].start_date_day.value   = startDates[2];
+        document.forms[0].start_date_year.value  = parseInt(startDates[0], 10);
+        document.forms[0].start_date_month.value = parseInt(startDates[1], 10);
+        document.forms[0].start_date_day.value   = parseInt(startDates[2], 10);
     }
 }
 
-$(document).ready(function(){
-   $('#dateSpan').change(function(){setDate(this.value)});
+$(document).ready(function() {
+   $('#date_span').change(function() { setDate(this.value) });
 });
