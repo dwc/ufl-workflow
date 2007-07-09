6069d33d640cb8f308028a07dd621674beb07c25
spooner <spooner@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-07-09 13:15:06 +0000

    Display only enabled processes.

--- UFL-Workflow/trunk/lib/UFL/Workflow/Controller/Root.pm
+++ UFL-Workflow/trunk/lib/UFL/Workflow/Controller/Root.pm
@@ -55,7 +55,8 @@ sub index : Path('') Args(0) {
 
     my $actions   = $c->user->pending_actions;
     my $requests  = $c->user->recent_requests;
-    my $processes = $c->model('DBIC::Process');
+    my $processes = $c->model('DBIC::Process')->search(
+    	{ 'enabled' => 1 });
 
     $c->stash(
         actions   => $actions,
