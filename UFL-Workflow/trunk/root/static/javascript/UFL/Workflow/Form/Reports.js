085c1d4befc39ba53131b4089797b45ff7cbe0e2
spooner <spooner@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-07-09 18:24:26 +0000

    Working prototype for Mike to continue with

--- UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
+++ UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
@@ -1,10 +1,9 @@
- $("h3").click(function(){
-   alert("Thanks for visiting!");
-   return false;
- });
-
-
-function setDate() {
-    console.log("Date Range Selected");
+function setDate(dateRange) {
+    console.log("Date Range Selected: " + dateRange);
 }
 
+$(document).ready(function(){
+   console.log("Document Ready");
+   $('#dateSpan').change(function(){setDate(this.value)});
+   console.log("Change Event Inserted");
+});
