a3400a426d8c3897ecdf1f56b5e36d9e6dfc26c2
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2007-07-19 18:48:34 +0000

    Namespace JavaScript

--- UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
+++ UFL-Workflow/trunk/root/static/javascript/UFL/Workflow/Form/Reports.js
@@ -1,59 +1,11 @@
-var groups = new Array();
+if (typeof UFL == 'undefined') UFL = {};
+if (typeof UFL.Workflow == 'undefined') UFL.Workflow = {};
+if (typeof UFL.Workflow.Form == 'undefined') UFL.Workflow.Form = {};
 
-function setDate(date) {
-    if (date) {
-        var dateParts = date.split('-', 3);
-        document.forms[0].start_date_year.value  = parseInt(dateParts[0], 10);
-        document.forms[0].start_date_month.value = parseInt(dateParts[1], 10);
-        document.forms[0].start_date_day.value   = parseInt(dateParts[2], 10);
-    }
-}
+UFL.Workflow.Form.Reports = function(url) {
+    var me = this;
+    var groups = new Array();
 
-function click(object, option, condition) {
-    if (option && object) {
-        switch(option) {
-            case "clear":
-                if (condition) {
-                    if (object.value == condition) {
-                        object.value = "";
-                    }
-                }
-                else {
-                    object.value = "";
-                }
-            break;
-        }
-    }
-}
-
-function keyupGroupSearch(object) {
-    var groupSelect = $("#group_id").empty();
-
-    var input = object.value.toLowerCase();
-    for (group in groups) {
-        var groupName = groups[group].toLowerCase();
-
-        if (groupName.indexOf(input) > -1) {
-            var option = new Option(groups[group], group);
-
-            var options = groupSelect.get(0).options;
-            options[options.length] = option;
-        }
-    }
-}
-
-function clickGroupListAll(object) {
-    var groupSelect = $("#group_id").empty();
-
-    for (group in groups) {
-        var option = new Option(groups[group], group);
-
-        var options = groupSelect.get(0).options;
-        options[options.length] = option;
-    }
-}
-
-function initialize(url) {
     $.getJSON(url, function(json) {
         if (json && json.groups && json.groups.length > 0) {
             for (group in json.groups) {
@@ -61,11 +13,64 @@ function initialize(url) {
             }
         }
     });
-}
 
-$(document).ready(function() {
-    $('#start_date').change(function() { setDate(this.value) });
-    $("#group_name").click(function() { click(this, 'clear', 'Search for group'); });
-    $("#group_name").keyup(function() { keyupGroupSearch(this) });
-    $("#group_list_all").click(function() { clickGroupListAll(this) });
-});
+    $(document).ready(function() {
+        $('#start_date').change(function() { me.setDate(this.value) });
+        $("#group_name").click(function() { me.click(this, 'clear', 'Search for group'); });
+        $("#group_name").keyup(function() { me.keyupGroupSearch(this) });
+        $("#group_list_all").click(function() { me.clickGroupListAll(this) });
+    });
+
+    this.setDate = function(date) {
+        if (date) {
+            var dateParts = date.split('-', 3);
+            document.forms[0].start_date_year.value  = parseInt(dateParts[0], 10);
+            document.forms[0].start_date_month.value = parseInt(dateParts[1], 10);
+            document.forms[0].start_date_day.value   = parseInt(dateParts[2], 10);
+        }
+    };
+
+    this.click = function(object, option, condition) {
+        if (option && object) {
+            switch (option) {
+                case "clear":
+                    if (condition) {
+                        if (object.value == condition) {
+                            object.value = "";
+                        }
+                    }
+                    else {
+                        object.value = "";
+                    }
+                break;
+            }
+        }
+    };
+
+    this.keyupGroupSearch = function(object) {
+        var groupSelect = $("#group_id").empty();
+
+        var input = object.value.toLowerCase();
+        for (group in groups) {
+            var groupName = groups[group].toLowerCase();
+
+                if (groupName.indexOf(input) > -1) {
+                var option = new Option(groups[group], group);
+
+                var options = groupSelect.get(0).options;
+                options[options.length] = option;
+            }
+        }
+    };
+
+    this.clickGroupListAll = function(object) {
+        var groupSelect = $("#group_id").empty();
+
+        for (group in groups) {
+            var option = new Option(groups[group], group);
+
+            var options = groupSelect.get(0).options;
+            options[options.length] = option;
+        }
+    };
+}
