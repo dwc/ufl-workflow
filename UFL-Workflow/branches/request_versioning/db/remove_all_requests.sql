66d3bae16b9a8abab636eaaed88ddc10e72d8867
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2008-04-14 19:32:07 +0000

    Fix table name

--- UFL-Workflow/branches/request_versioning/db/remove_all_requests.sql
+++ UFL-Workflow/branches/request_versioning/db/remove_all_requests.sql
@@ -2,5 +2,5 @@ DELETE FROM action_groups;
 DELETE FROM actions;
 DELETE FROM documents;
 DELETE FROM field_data;
-DELETE FROM requests_versions;
+DELETE FROM request_versions;
 DELETE FROM requests;
