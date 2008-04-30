62a1950104072440bbbaa96feb6f321621feea21
chetanmurthy <chetanmurthy@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2008-04-30 13:01:10 +0000

    code review changes

--- UFL-Workflow/branches/recycle_dropdown/root/static/javascript/UFL/Workflow/Form/StatusGroupAssignment.js
+++ UFL-Workflow/branches/recycle_dropdown/root/static/javascript/UFL/Workflow/Form/StatusGroupAssignment.js
@@ -17,39 +17,30 @@ UFL.Workflow.Form.StatusGroupAssignment = function(url, statusId, groupId) {
         }
     });
 
-    function sort_groups(a, b){
-      var x = a.name.toLowerCase();
-      var y = b.name.toLowerCase();
-      return ((x<y) ? -1 : ((x>y) ? 1 : 0));
-    }
-
     this.getActionGroups = function() {
         $.getJSON(url, statusSelect.serialize(), function(json) {
             groupSelect.empty();
 
             if (json && json.groups && json.groups.length > 0) {
-                json.groups.sort(sort_groups);
                 var j = 0;
-                if (json.prev_recycle_group){
-                    var option = new Option(json.prev_recycle_group.name, json.prev_recycle_group.id);
+                if (json.previous_group) {
+                    var option = new Option(json.previous_group.name, json.previous_group.id);
                     option.selected = true;
-                    groupSelect.get(0).options[j] = option;
-                    j++;
-                }
-                for (var i = 0; i < json.groups.length; i++) {
-                    var group = json.groups[i];
-
-                    if (json.prev_recycle_group && group.id == json.prev_recycle_group.id){
-                        continue;
-                    }
-                    var option = new Option(group.name, group.id);
-                    if ( ! json.prev_recycle_group && json.selected_group && group.id == json.selected_group.id) {
-                        option.selected = true;
-                    }
-
                     groupSelect.get(0).options[j++] = option;
                 }
 
+                for (var i = 0; i < json.groups.length; i++) {
+                    var group = json.groups[i];
+                    if (json.previous_group && group.id == json.previous_group.id) {
+                        continue;
+                    }
+
+                    var option = new Option(group.name, group.id);
+                    if ( ! json.previous_group && json.selected_group && group.id == json.selected_group.id) {
+                        option.selected = true;
+                    }
+                    groupSelect.get(0).options[j++] = option;
+                }
                 groupSelect.parent().show();
             }
             else {
