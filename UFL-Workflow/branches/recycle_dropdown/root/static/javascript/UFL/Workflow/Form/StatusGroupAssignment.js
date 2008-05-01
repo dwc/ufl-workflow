755ddb30eb202bde2a06135840d1be07fe7e45f4
dwc <dwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2008-05-01 22:39:47 +0000

    I changed my mind about the name of this; prev_group is more consistent with the rest of the app

--- UFL-Workflow/branches/recycle_dropdown/root/static/javascript/UFL/Workflow/Form/StatusGroupAssignment.js
+++ UFL-Workflow/branches/recycle_dropdown/root/static/javascript/UFL/Workflow/Form/StatusGroupAssignment.js
@@ -23,20 +23,20 @@ UFL.Workflow.Form.StatusGroupAssignment = function(url, statusId, groupId) {
 
             if (json && json.groups && json.groups.length > 0) {
                 var j = 0;
-                if (json.previous_group) {
-                    var option = new Option(json.previous_group.name, json.previous_group.id);
+                if (json.prev_group) {
+                    var option = new Option(json.prev_group.name, json.prev_group.id);
                     option.selected = true;
                     groupSelect.get(0).options[j++] = option;
                 }
 
                 for (var i = 0; i < json.groups.length; i++) {
                     var group = json.groups[i];
-                    if (json.previous_group && group.id == json.previous_group.id) {
+                    if (json.prev_group && group.id == json.prev_group.id) {
                         continue;
                     }
 
                     var option = new Option(group.name, group.id);
-                    if (! json.previous_group && json.selected_group && group.id == json.selected_group.id) {
+                    if (! json.prev_group && json.selected_group && group.id == json.selected_group.id) {
                         option.selected = true;
                     }
 
