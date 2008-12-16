42f38ed8e8e67d573fe057af4d3a8fe64ca950cf
spooner <spooner@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2008-12-16 22:11:29 +0000

    Tagging 0.32

---
+++ UFL-Workflow/tags/0.32/lib/UFL/Workflow/View/JSON.pm
@@ -0,0 +1,34 @@
+package UFL::Workflow::View::JSON;
+
+use strict;
+use warnings;
+use base qw/Catalyst::View::JSON/;
+
+=head1 NAME
+
+UFL::Workflow::View::JSON - JSON view component
+
+=head1 SYNOPSIS
+
+See L<UFL::Workflow>.
+
+=head1 DESCRIPTION
+
+L<JSON> view component.
+
+=head1 SEE ALSO
+
+L<Catalyst::View::JSON>
+
+=head1 AUTHOR
+
+Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
