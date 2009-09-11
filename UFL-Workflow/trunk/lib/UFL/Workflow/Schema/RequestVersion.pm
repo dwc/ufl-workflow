0c816eb7095eadaf0ddd5713c284484584128d69
spooner <spooner@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2009-09-11 19:57:52 +0000

    Cleaning up per code review.

--- UFL-Workflow/trunk/lib/UFL/Workflow/Schema/RequestVersion.pm
+++ UFL-Workflow/trunk/lib/UFL/Workflow/Schema/RequestVersion.pm
@@ -42,7 +42,7 @@ __PACKAGE__->belongs_to(
 
 =head1 NAME
 
-UFL::Workflow::Schema::RequestVersion - RequestVersion table class
+UFL::Workflow::Schema::RequestVersion - Request version table class
 
 =head1 SYNOPSIS
 
@@ -50,12 +50,17 @@ See L<UFL::Workflow>.
 
 =head1 DESCRIPTION
 
-RequestVersion table class for L<UFL::Workflow::Schema>.
+Request version table class for L<UFL::Workflow::Schema>.
 
 
 =head2 uri_args
 
-Return the list of URI path arguments needed to identify this requestVersion.
+Return the L<UFL::Workflow::Schema::Step> in the
+L<UFL::Workflow::Schema::Process> associated with the first
+L<UFL::Workflow::Schema::Action> on this request.
+
+
+Return the list of URI path arguments needed to identify the L<UFL::Workflow::Schema::RequestVersion> of the L<UFL::Workflow::Schema::Request>.
 
 =cut
 
@@ -67,7 +72,7 @@ sub uri_args {
 
 =head1 AUTHOR
 
-Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+Joey Spooner<lt>spooner@ufl.eduE<gt>
 
 =head1 LICENSE
 
