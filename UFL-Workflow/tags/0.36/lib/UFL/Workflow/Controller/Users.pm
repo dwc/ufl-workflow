eb0893aa02bff18ceebf315a22b1581a44879d2a
danieltwc <danieltwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2009-09-15 22:09:29 +0000

    Retroactively tagging 0.36

---
+++ UFL-Workflow/tags/0.36/lib/UFL/Workflow/Controller/Users.pm
@@ -0,0 +1,271 @@
+package UFL::Workflow::Controller::Users;
+
+use strict;
+use warnings;
+use base qw/UFL::Workflow::BaseController/;
+
+=head1 NAME
+
+UFL::Workflow::Controller::Users - Users controller component
+
+=head1 SYNOPSIS
+
+See L<UFL::Workflow>.
+
+=head1 DESCRIPTION
+
+L<Catalyst> controller component for managing users.
+
+=head1 METHODS
+
+=head2 index
+
+Display a list of current users.
+
+=cut
+
+sub index : Path('') Args(0) {
+    my ($self, $c) = @_;
+
+    my $letter;
+    my $query;
+    my $results;
+    my $users;
+
+    if ($letter = $c->req->query_parameters->{'letter'}) {
+        $users = $c->model('DBIC::User')->search({ "LOWER(username)" => { 'like', $letter . '%'  } }, { order_by => 'username' });
+    }
+    else {
+        $users = $c->model('DBIC::User')->search({ "LOWER(username)" => { 'like', 'a%'  } }, { order_by => 'username' });
+        $letter = 'a';
+    }
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+
+        if ($query = $result->valid('query')) {
+            $results = $c->model('DBIC::User')->search({ 'LOWER(username)' => { 'like', $query . '%' } }, { order_by => 'username' });
+            $letter = substr($query,0,1);
+            $users = $c->model('DBIC::User')->search({ "LOWER(username)" => { 'like', $letter . '%'  } }, { order_by => 'username' });
+        }
+    }
+
+    $c->stash(
+        letter    => $letter,
+        query     => $query,
+        results   => $results,
+        template  => 'users/index.tt',
+        users     => $users,
+    );
+}
+
+=head2 add
+
+Add one or more new users.
+
+=cut
+
+sub add : Local {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my @new_users = split /[ \r\n]+/, lc $result->valid('users');
+
+            my (@added_users, @existing_users, @invalid_users);
+            foreach my $new_user (@new_users) {
+                # Remove the hyphen in the UFID to match LDAP
+                if ($new_user =~ /\d{4}-\d{4}/) {
+                    $new_user =~ s/-//;
+                }
+
+                my $attribute = $new_user =~ /\d{8}/ ? 'uflEduUniversityId' : 'uid';
+                if (my $entry = $c->model('LDAP')->search("($attribute=$new_user)")->shift_entry) {
+                    if (my $user = $c->model('DBIC::User')->find({ username => $entry->uid })) {
+                        push @existing_users, $user;
+                    }
+                    else {
+                        my $user = $c->model('DBIC::User')->create({ username => $entry->uid });
+                        push @added_users, $user;
+                    }
+                }
+                else { 
+                    push @invalid_users, $new_user;
+                }
+            }
+
+            $c->stash(
+                added_users    => [ @added_users ],
+                existing_users => [ @existing_users ],
+                invalid_users  => [ @invalid_users ],
+            );
+        }
+    }
+
+    $c->stash(template => 'users/add.tt');
+}
+
+=head2 user
+
+Fetch the specified user.
+
+=cut
+
+sub user : PathPart('users') Chained('/') CaptureArgs(1) {
+    my ($self, $c, $username) = @_;
+
+    my $user = $c->model('DBIC::User')->find({ username => $username });
+    $c->detach('/default') unless $user;
+    $c->detach('/forbidden') unless $c->user->username eq $user->username
+        or $c->check_any_user_role('Administrator', 'Help Desk');
+
+    $c->stash(user => $user);
+}
+
+=head2 view
+
+Display basic information on the stashed user.
+
+=cut
+
+sub view : PathPart('') Chained('user') Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash(template => 'users/view.tt');
+}
+
+=head2 edit
+
+Edit the user's information, including whether they want to receive
+email or not.
+
+=cut
+
+sub edit : PathPart Chained('user') Args(0) {
+    my ($self, $c) = @_;
+
+    my $user = $c->stash->{user};
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            $user->update({
+                wants_email => $result->valid('wants_email') ? 1 : 0,
+            });
+        }
+    }
+
+    $c->res->redirect($c->uri_for($self->action_for('view'), $user->uri_args));
+}
+
+=head2 add_group_role
+
+Add the stashed user to the specified group-role.
+
+=cut
+
+sub add_group_role : PathPart Chained('user') Args(0) {
+    my ($self, $c) = @_;
+
+    if ($c->req->method eq 'POST') {
+        my $result = $self->validate_form($c);
+        if ($result->success) {
+            my $group_role = $c->model('DBIC::GroupRole')->find({
+                group_id => $result->valid('group_id'),
+                role_id  => $result->valid('role_id'),
+            });
+            $c->detach('/default') unless $group_role;
+
+            my $user = $c->stash->{user};
+            $user->add_to_group_roles($group_role) unless $user->has_group_role($group_role);
+
+            return $c->res->redirect($c->uri_for($self->action_for('view'), $user->uri_args));
+        }
+    }
+
+    my $groups = $c->model('DBIC::Group')->root_groups;
+
+    # Show the roles once a group is selected
+    if (my $group_id = $c->req->param('group_id')) {
+        $group_id =~ s/\D//g;
+        $c->detach('/default') unless $group_id;
+
+        my $group = $c->model('DBIC::Group')->find($group_id);
+        $c->detach('/default') unless $group;
+
+        $c->stash(group => $group);
+    }
+
+    $c->stash(
+        groups   => $groups,
+        template => 'users/add_group_role.tt',
+    );
+}
+
+=head2 list_group_roles
+
+List roles that are valid for the user, and the specified
+group via L<JSON>.
+
+=cut
+
+sub list_group_roles : PathPart Chained('user') Args(0) {
+    my ($self, $c) = @_;
+   
+    # Show the roles once a group is selected
+    if (my $group_id = $c->req->param('group_id')) {
+        $group_id =~ s/\D//g;
+        $c->detach('/default') unless $group_id;
+
+        my $group = $c->model('DBIC::Group')->find($group_id);
+        $c->detach('/default') unless $group;
+
+        my @roles = $group->roles;
+        $c->stash(roles => [ map { $_->to_json } @roles ]);
+    }
+
+    my $view = $c->view('JSON');
+    $view->expose_stash([ qw/roles/ ]);
+    $c->forward($view);
+}
+
+=head2 delete_group_role
+
+Remove the stashed user from the specified group-role.
+
+=cut
+
+sub delete_group_role : PathPart Chained('user') Args(0) {
+    my ($self, $c) = @_;
+
+    die 'Method must be POST' unless $c->req->method eq 'POST';
+
+    my $user = $c->stash->{user};
+
+    my $result = $self->validate_form($c);
+    if ($result->success) {
+        my $group_role = $c->model('DBIC::GroupRole')->find({
+            group_id => $result->valid('group_id'),
+            role_id  => $result->valid('role_id'),
+        });
+        $c->detach('/default') unless $group_role;
+
+        $user->remove_from_group_roles($group_role);
+    }
+
+    return $c->res->redirect($c->uri_for($self->action_for('view'), $user->uri_args));
+}
+
+=head1 AUTHOR
+
+Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ UFL-Workflow/tags/0.36/root/static/javascript/jquery.jsonsuggest.js
@@ -0,0 +1,195 @@
+(function() {
+
+    jQuery.fn.jsonSuggest = function(optionSettings) {
+
+	// DEFAULT SETTINGS
+        var searchField = '';
+        var searchResultId = '';
+        var settings = '';                    
+
+        var defaults = {
+            url: '',
+            suggestId: 'suggest_',
+            resultName: 'name',
+            resultId: 'id',
+            suggestListStyle: 'none',
+            suggestMargin: '0',
+            suggestPadding: '0',
+            suggestFontSize: '12px',
+            suggestPosition: 'absolute',
+            suggestZIndex: '5',
+            suggestBackgroundColor: '#fff',
+            suggestBorder: '1px solid #000',
+            suggestLiPadding: '2px 0 0 5px',
+            suggestLiMargin: '0px',
+            suggestSelectedClass: 'jsonSuggestSelected'
+	 }
+		
+         // RESULT BEHAVIOR
+         var clearResults = function() {
+	     searchResultContainer = searchResultId;
+             $(searchResultContainer).hide();
+             $(searchResultContainer).empty();
+	 }
+
+         var displayResults = function(results) {
+  	     searchIdWidth = $(searchField).css("width");
+             $(searchResultId).css("width", searchIdWidth);
+
+             position = $(searchField).position();
+	     height = $(searchField).outerHeight(true);
+	     marginBottom = parseInt($(searchField).css("margin-bottom"));
+             top = ((position.top + height) - marginBottom);
+
+             $(searchResultId).css("position", "absolute");
+             $(searchResultId).css("top", top);
+             $(searchResultId).css("left", position.left);
+             $(searchResultId).css("list-style", settings.suggestListStyle);
+	     $(searchResultId).css("margin", settings.suggestMargin);
+             $(searchResultId).css("z-index", settings.suggestZIdex);
+             $(searchResultId).css("position", settings.suggestPosition);
+             $(searchResultId).css("background-color", settings.suggestBackgroundColor);
+             $(searchResultId).css("border", settings.suggestBorder);
+            
+	     if(results) {
+                 if(results.roles != null && results.roles.length > 0) {
+                     $(searchResultId).empty();
+
+                     jQuery(results.roles).each(function(i, result) {
+                         $(searchResultId).append("<li id='" + settings.suggestId  + result.id + "'>" + result.name + "</li>");
+                     });
+
+                     $(searchResultId + " > li").css("padding", settings.suggestLiPadding);
+                     $(searchResultId + " > li").css("margin", settings.suggestLiMargin);
+
+                     $(searchResultId).show();
+                 }
+	     }
+
+             $(searchResultId + " > li").mouseover(function() {
+                 $(searchResultId + " > li").removeClass(settings.suggestSelectedClass);
+                 $(this).addClass(settings.suggestSelectedClass);
+             });
+
+             $(searchResultId + " > li").click(function(e) {
+		 selectItem(searchResultId, getSelectedIndex());
+	         clearResults();
+             });
+
+         }
+
+	 // replace this with the levenshtein sort
+         var sortResults = function(a,b) {
+             a = a[1];
+             b = b[1];
+
+             return a == b ? 0 : (a < b ? -1 : 1);
+         }
+
+	// KEYBOARD AND MOUSE BEHAVIOR
+        var selectItem = function(listId, index) {
+            var count = 0;
+            $(listId + " > li").removeClass(settings.suggestSelectedClass);
+
+            if(index != -1) {
+                $(listId + " > li").each(function() {
+                    if(index == count) {
+                        $(this).addClass(settings.suggestSelectedClass);
+			$(searchField).attr("value",this.innerHTML);
+                    }
+
+                    count++;
+                });
+            }
+            else {
+                $(searchField).attr("value", query);
+            }
+        }
+
+	var getSelectedIndex = function() {
+            var count = 0;
+            var selectedItemIndex = -1;
+
+            $(searchResultId + " > li").each(function() {
+                if($(this).hasClass(settings.suggestSelectedClass)) {
+                    selectedItemIndex = count;
+                }
+                count++;
+	    });
+
+            return selectedItemIndex;
+	}
+     
+        var keySwitch = function(charCode) {
+            if(charCode.keyCode == 38 || charCode.keyCode == 39 || charCode.keyCode == 40) {
+                var selectedIndex = getSelectedIndex();
+
+                switch(charCode.keyCode) {
+                    case 38:
+                        if(selectedIndex >= 0) { selectedIndex--; }
+                        selectItem(searchResultId, selectedIndex);
+	                break;
+
+                    case 39:
+                        selectItem(searchResultId, selectedIndex);
+                        clearResults();
+                        break;
+
+                    case 40:
+                        selectedIndex++;
+                        selectItem(searchResultId, selectedIndex);
+                        break;
+                }           
+            }
+	    else {
+ 	        $(searchResultId).empty();
+                $(searchResultId).hide();
+                search(charCode);
+ 	    }
+        }
+
+	// SEARCH PROCESSING
+        function search(charCode) {
+            query = $(searchField).attr("value");
+
+            if(query != '') {
+                $.getJSON(settings.url + "?q=" + query, function(results) {
+			displayResults(results);
+		});
+	    }
+            else {
+                clearResults();
+            }
+	}
+
+	// INITIALIZATION
+        var initialize = function() {
+            settings = jQuery.extend(defaults, optionSettings);
+            searchResultId = "#" + settings.suggestId;
+
+            $(searchField).attr("autocomplete","off");
+
+            $(searchField).keyup(function (e) { 
+	        keySwitch(e); 
+            });
+
+	    $("head").append("<style type='text/css' media='screen'>.hintbox_list_container { display: block; } .jsonSuggestSelected { background-color:#D1DFFD; }</style>");
+
+            $(searchField).click(function() {
+		    this.value = "";
+            });
+
+	    $(window).resize(function() {
+                displayResults();
+            });            
+
+            $(searchField).after("<ul id='" + settings.suggestId + "'></ul>");
+	    clearResults();
+        }
+
+        if(this.length == 1) {
+            searchField = "#" + this[0].id;
+            initialize();
+        }
+    }
+})();
