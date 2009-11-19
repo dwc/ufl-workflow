380baa81e0fd0809f19584f4b6f3b57f264c390c
danieltwc <danieltwc@aa130be4-a239-11de-ba4d-5dbaeb624ccf>
2009-11-19 20:27:21 +0000

    Tagging 0.39

---
+++ UFL-Workflow/tags/0.39/root/static/javascript/UFL/Workflow/Form/StatusGroupAssignment.js
@@ -0,0 +1,43 @@
+if (typeof UFL == 'undefined') UFL = {};
+if (typeof UFL.Workflow == 'undefined') UFL.Workflow = {};
+if (typeof UFL.Workflow.Form == 'undefined') UFL.Workflow.Form = {};
+
+UFL.Workflow.Form.StatusGroupAssignment = function(url, statusId, groupId) {
+    var me = this;
+    var statusSelect;
+    var groupSelect;
+
+    $(document).ready(function() {
+        statusSelect = $("#" + statusId);
+        groupSelect = $("#" + groupId);
+
+        if (groupSelect && statusSelect) {
+            groupSelect.parent().hide();
+            statusSelect.change(me.getActionGroups);
+        }
+    });
+
+    this.getActionGroups = function() {
+        $.getJSON(url, statusSelect.serialize(), function(json) {
+            groupSelect.empty();
+
+            if (json && json.groups && json.groups.length > 0) {
+                for (var i = 0; i < json.groups.length; i++) {
+                    var group = json.groups[i];
+
+                    var option = new Option(group.name, group.id);
+                    if (json.selected_group && group.id == json.selected_group.id) {
+                        option.selected = true;
+                    }
+
+                    groupSelect.get(0).options[i] = option;
+                }
+
+                groupSelect.parent().show();
+            }
+            else {
+                groupSelect.parent().hide();
+            }
+        });
+    }
+}
